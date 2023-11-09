430f27e5b55b94c5d865ffa1ea4f49be7e87e6e3
yoonseong <zxczxc0011@naver.com>
2023-11-09 15:23:29 +0900

    Accounts Profile 기능 구현

--- accounts/views.py
+++ accounts/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from .forms import CustomUserCreationForm, CustomAuthenticationForm
 from django.contrib.auth import login as auth_login
 from django.contrib.auth import logout as auth_logout
-
+from .models import User
 # Create your views here.
 
 def signup(request):
@@ -40,3 +40,12 @@ def login(request):
 def logout(request):
     auth_logout(request)
     return redirect('accounts:login')
+
+def profile(request, username):
+    user_info = User.objects.get(username=username)
+
+    context = {
+        'user_info' : user_info,
+    }
+
+    return render(request, 'profile.html', context)
